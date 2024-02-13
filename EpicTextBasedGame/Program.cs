63d7c064f8e474bc6d78f4c5e64ea32fe04ba48f
HolyCowOfDoom <ut.ozyurt@gmail.com>
2024-02-13 13:38:57 +0100

    moo
    moo

---
+++ EpicTextBasedGame/Program.cs
@@ -0,0 +1,18 @@
+﻿public static class Program{
+    public static void Main(){
+        Cow utku = new("Utku");
+        utku.Moo();
+    }
+
+    
+}
+
+public class Cow{
+    string Name;
+    public Cow(string name){
+        Name = name;
+    }
+    public void Moo(){
+        Console.WriteLine("Moo!");
+    }
+}
