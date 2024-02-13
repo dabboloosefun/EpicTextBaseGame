4acc48d363b43031eb9160e2c5f6d812d629f3a8
HolyCowOfDoom <ut.ozyurt@gmail.com>
2024-02-13 15:45:31 +0100

    updated Weapon.cs
    World.cs has Weapon() calls, passing it 3 arguments. I assume the third argument is something like 'Stength'. not sure though

--- EpicTextBasedGame/Weapon.cs
+++ EpicTextBasedGame/Weapon.cs
@@ -1,4 +1,12 @@
 public class Weapon
 {
+    int ID;
+    string Name;
+    int Strength; //? Weapon() call in World.cs gives 3 arguments
+    public Weapon(int id, string name, int strength){
+        ID = id;
+        Name = name;
+        Strength = strength;
+    }
 
 }
