1a98d80df7ef72d3776b7693090bf63f960de183
dabboloosefun <146081681+dabboloosefun@users.noreply.github.com>
2024-02-13 15:23:20 +0100

    A little clean up.

--- EpicTextBasedGame/Main.cs
+++ EpicTextBasedGame/Main.cs
@@ -1,39 +1,5 @@
-﻿
-public static class Program
+﻿public static void Main()
 {
-    public static void Main()
-    {
-        Cow utku = new Cow("Utku");
-        utku.Moo();
-        Gator jerry = new Gator(51);
-        jerry.Kill();
-        Gator bob = new Gator(49);
-        bob.Kill();
-    }
-
-    
+    // Our main code code here, preferably everything is dynamic (so lots of methods).
 }
-
-public class Cow{
-    string Name;
-    public Cow(string name){
-        Name = name;
-    }
-    public void Moo(){
-        Console.WriteLine("Moo!");
-    }
-}
-
-public class Gator
-{
-    int Deadlyness;
-    public Gator(int deadlyness)
-    {
-        Deadlyness = deadlyness;
-    }
-    public void Kill()
-    {
-        Console.WriteLine(Deadlyness > 50 ? "He rips your throat" : "He a shleepy boi");
-    }
-}
-
+// All algortithms / methods go here
