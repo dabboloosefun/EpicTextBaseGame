827232bd1e8808de0aedee9bcb9d9e0a34f10b75
HolyCowOfDoom <ut.ozyurt@gmail.com>
2024-02-13 14:10:17 +0100

    fixed main sandwich
    there was a main sandwich. fixed it

--- EpicTextBasedGame/obj/Debug/net8.0/EpicTextBasedGame.AssemblyInfo.cs
+++ EpicTextBasedGame/obj/Debug/net8.0/EpicTextBasedGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EpicTextBasedGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa4e612fe39506eb42987c6ec3ee6eb4677b0c34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c824e5beadde18521d1b9635194bb9b7300242cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EpicTextBasedGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EpicTextBasedGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
