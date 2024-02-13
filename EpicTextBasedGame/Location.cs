48807dd9b0ae6ab68b1cd9aee76190fc8d678854
HolyCowOfDoom <ut.ozyurt@gmail.com>
2024-02-13 15:33:36 +0100

    location.cs
    location.cs

---
+++ EpicTextBasedGame/Location.cs
@@ -0,0 +1,15 @@
+public class Location{
+    public int ID;
+    public string Name;
+    public string Description;
+    public Quest QuestAvailableHere;
+    public Monster MonsterLivingHere;
+    public Location LocationToNorth;
+    public Location LocationToEast;
+    public Location LocationToSouth;
+    public Location LocationToWest;
+
+    public Location(int id, string name, string description, Quest quest, Monster monster){
+
+    }
+}
