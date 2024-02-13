c824e5beadde18521d1b9635194bb9b7300242cc
dabboloosefun <146081681+dabboloosefun@users.noreply.github.com>
2024-02-13 14:01:00 +0100

    Cleanup

---
+++ EpicTextBasedGame/Main.cs
@@ -0,0 +1,40 @@
+﻿public static void Main()
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Cow utku = new Cow("Utku");
+            utku.Moo();
+            Gator jerry = new Gator(51);
+            jerry.Kill();
+            Gator bob = new Gator(49);
+            bob.Kill();
+        }
+
+        
+    }
+
+    public class Cow{
+        string Name;
+        public Cow(string name){
+            Name = name;
+        }
+        public void Moo(){
+            Console.WriteLine("Moo!");
+        }
+    }
+
+    public class Gator
+    {
+        int Deadlyness;
+        public Gator(int deadlyness)
+        {
+            Deadlyness = deadlyness;
+        }
+        public void Kill()
+        {
+            Console.WriteLine(Deadlyness > 50 ? "He rips your throat" : "He a shleepy boi");
+        }
+    }
+}
