6012a419d3ed0d32ad0c8cacea95f911c05b911e
dabboloosefun <juliankreugel@gmail.com>
2024-02-14 10:24:49 +0100

    Final quickie

--- EpicTextBasedGame/Weapon.cs
+++ EpicTextBasedGame/Weapon.cs
@@ -1,8 +1,8 @@
 public class Weapon
 {
-    int ID;
-    string Name;
-    int MaximumDamage; //? Weapon() call in World.cs gives 3 arguments
+    public int ID;
+    public string Name;
+    public int MaximumDamage; //? Weapon() call in World.cs gives 3 arguments
     public Weapon(int id, string name, int maximumDamage){
         ID = id;
         Name = name;
