2e57c5010f2189faa4ce232c6974b5ecf5d43724
HolyCowOfDoom <ut.ozyurt@gmail.com>
2024-02-14 11:53:22 +0100

    location.cs and quest.cs

--- EpicTextBasedGame/Quest.cs
+++ EpicTextBasedGame/Quest.cs
@@ -1,3 +1,34 @@
 public class Quest
 {
+    int ID;
+    string Name;
+    string Description;
+    bool Cleared = false;
+    public Quest(int id, string name, string description)
+    {
+        ID = id;
+        Name = name;
+        Description = description;
+    }
+
+    public void ClearQuest(Player player, string bonusModifier = "")
+    {
+        Cleared = true;
+
+        if(bonusModifier == "Within3Hits")
+        {
+            Player.Inventory.Add("Jar Of Llightning");
+        }
+
+        if(bonusModifier == "SparedGoblin")
+        {
+            Player.Inventory.Add("Goblin's Blessing");
+        }
+
+        if(bonusModifier == "SlayedGoblin")
+        {
+            Player.CurrentHealth -= 1;
+            Player.Inventory.Add("Goblin's Curse");
+        }
+    }
 }
