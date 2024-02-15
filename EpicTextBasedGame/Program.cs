7b6019ba40eea3ef1b47a88a8fcd754fef6ee2eb
HolyCowOfDoom <ut.ozyurt@gmail.com>
2024-02-15 10:38:51 +0100

    another bug

--- EpicTextBasedGame/Program.cs
+++ EpicTextBasedGame/Program.cs
@@ -4,6 +4,6 @@
     {
         // Our main code code here, preferably everything is dynamic (so lots of methods).
         Monster Bob = new Monster(1, "Sick Goblin", 5, 5, 10);
-        Bob.DisplayStats;
+        Bob.DisplayStats();
     }
 }
