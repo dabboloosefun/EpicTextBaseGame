6dad616dc3d2c034c43fcaa57e98e50e7c651cb1
Aortus <thijs1506@gmail.com>
2024-02-17 16:09:13 +0100

    Update SuperAdventure.cs
    Er staat nog veel in comment wat nog moet gebeuren

--- EpicTextBasedGame/SuperAdventure.cs
+++ EpicTextBasedGame/SuperAdventure.cs
@@ -1,3 +1,121 @@
 public class SuperAdventure
 {
+    public static void Fight(Player player, Monster monster)
+    {
+        Console.WriteLine($"A {monster.Name} has appeared");
+        bool playerturn = true;
+        while (player.CurrentHealth > 0 && monster.CurrentHealth > 0)
+        {
+            if (playerturn){
+            bool Actiondone = false;
+            while(!Actiondone){
+                monster.DisplayStats();
+                Console.WriteLine("1. Attack");
+                Console.WriteLine("2. Heal");
+                Console.WriteLine("3. Spell");
+                Console.WriteLine("What do you want to do?");
+                    string attack_input = Console.ReadLine().ToLower();
+                    switch (attack_input){
+                    
+                    case "1":
+                    case "attack":
+                        monster.TakeDamage(player.CurrentWeapon.RollDamage());
+                        Actiondone = true;
+                        break;
+
+/*
+                    case "2":
+                    case "heal":
+                    if (player.Items.Contains("Potion")){ 
+                        player.RegenarateHealth(50)
+                        Actiondone = true;
+                        break;
+                    }
+                    else{
+                        Console.WriteLine("You do not have a way to heal yourself. Good luck with your low health!")
+                        break;
+                    }
+            }
+*/
+/*
+                    case "3":
+                    case "Spell":
+                        Console.WriteLine("What spell do you wish to cast?");
+                        Console.WriteLine("1. Buff (Increases your attacking power).");
+                        Console.WriteLine("2. Debuff the attack of the enemy");
+                        string spell_input = Console.ReadLine();
+                        if (spell_input == "1" || spell_input == "buff"){
+
+                        }
+                        else if (spell_input == "2" || spell_input == "debuff"){
+
+                        }
+*/        
+                    }
+                }
+            } // While loop voor player turn
+            else if (!playerturn)
+            {   
+                bool Mactiondone = false;
+                while (!Mactiondone){
+                int Monster_choice;
+                string Monster_action = "";
+                
+                if (monster.CurrentHealth < monster.MaxHealth / 2)
+                {
+                    Random rand = new Random();
+                    Monster_choice = rand.Next(1, 81);
+                }
+                else
+                {
+                    Random rand = new Random();
+                    Monster_choice = rand.Next(1, 101);
+                }
+
+                if (Monster_choice <= 60)
+                {
+                    Monster_action = "Attack";
+                }
+                else if (Monster_choice <= 80 && Monster_choice > 60)
+                {
+                    Monster_action = "Heal";
+                }
+                else if (Monster_choice > 80)
+                {
+                    if (Monster_choice > 90)
+                    {
+                        Monster_action = "Debuff";
+                    }
+                    else
+                    {
+                        Monster_action = "Buff";
+                    }
+                }
+
+                switch (Monster_action)
+                {
+                    case "Attack":
+                        // Hier moet een roll damage van monster komen
+                        monster.TakeDamage(10);
+                        Mactiondone = true;
+                        break;
+                    case "Heal":
+                        int regenamount = monster.CurrentHealth - monster.MaxHealth / 5;
+                        monster.RegenarateHealth(regenamount);
+                        Mactiondone = true;
+                        break;
+                    case "Debuff":
+
+                        Mactiondone = true;
+                        break;
+                    case "Buff":
+
+                        Mactiondone = true;
+                        break;
+                }
+                }
+            } // While loop voor enemy
+            playerturn = !playerturn; // Draait player turn om
+        } // While loop voor fight
+    }
 }
