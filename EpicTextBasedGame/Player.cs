47910d6e9e7f5f0e8dc96d1f644df175e650d536
HolyCowOfDoom <ut.ozyurt@gmail.com>
2024-02-17 11:28:59 +0100

    adding ListItems() to Player.cs

--- EpicTextBasedGame/Player.cs
+++ EpicTextBasedGame/Player.cs
@@ -9,10 +9,20 @@ public class Player
         Items = new List<string>();
     }
 
+    public void ListItems(){
+        Console.WriteLine("You are carrying these items in your inventory:");
+        for(int i = 0; i<Items.Count; i++) //we can just use the index in the list to select a new weapon with
+        {;
+            Console.WriteLine($"{i}. {Items[i]}:\n"); 
+        }
+    }
+
     public void ListWeapons()
     {
+        //possibly a null reference if there is no Weapon equiped
         Console.WriteLine($"Equiped weapon: {CurrentWeapon.Name}: Max damage: {CurrentWeapon.MaxDamage}, Crit chance: {CurrentWeapon.CritChance}\n");
-        for(int i = 0; i<Weapons.Count(); i++) //we can just use the index in the list to select a new weapon with
+        Console.WriteLine("You are carrying these weapons in your inventory:");
+        for(int i = 0; i<Weapons.Count; i++)
         {
             Weapon currentWeapon = Weapons[i];
             Console.WriteLine($"{i}. {currentWeapon.Name}: Max damage: {currentWeapon.MaxDamage}, Crit chance: {currentWeapon.CritChance}\n"); 
@@ -37,10 +47,10 @@ public class Player
             bool succesfulParse;
             do{
                 
-                Console.WriteLine($"Enter the number of the weapon you wish to equip. 0-{Weapons.Count()-1}");
+                Console.WriteLine($"Enter the number of the weapon you wish to equip. 0-{Weapons.Count-1}");
                 userSelectInput = Console.ReadLine();
                 succesfulParse = int.TryParse(userSelectInput, out selectedNumber);
-            } while (!succesfulParse || !(0 <= selectedNumber && selectedNumber < Weapons.Count())); //if Weapons.Count() = 3, valid inputs are 0, 1, 2. < is exclusive
+            } while (!succesfulParse || !(0 <= selectedNumber && selectedNumber < Weapons.Count)); //if Weapons.Count() = 3, valid inputs are 0, 1, 2. < is exclusive
             Weapons.Add(CurrentWeapon);
             CurrentWeapon = Weapons[selectedNumber];
             Weapons.Remove(Weapons[selectedNumber]); //even if weapons have the same name, they still have different ID so this hould work > Why remove the weapon from the list? You still have it in your inv
