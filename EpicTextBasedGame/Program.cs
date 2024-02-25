c8a5559201a89256ec3f81a9776e338c1dc7b4f7
ZeroTomato01 <146572680+ZeroTomato01@users.noreply.github.com>
2024-02-25 16:32:04 +0100

    Bit of cleanup in world and program, Added quests and monsters to their location instance creation.

--- EpicTextBasedGame/Program.cs
+++ EpicTextBasedGame/Program.cs
@@ -3,8 +3,7 @@
     
     public static void Main()
     {
-        
-        // Our main code code here, preferably everything is dynamic (so lots of methods).
+      
         //Effect critChaceBuffEffect3T = new Effect("BuffCritChance0.2_3T", 0.2, EffectTypes.BUFFCRITCHANCE, 3);
         //Item CritPotion3T = new Item(1, "CritPotion", critChaceBuffEffect3T, "increases crit chance by 0.2. lasts 3 turns");
         //Effect HealOverTimeEffect3T = new Effect("HealOverTime2_3T", 2, EffectTypes.HEALOVERTIME, 3);
@@ -30,14 +29,10 @@
         bool victory = false;
         Helper.TitleScreen();
 
-        // Little intro can go here.
-
         while (!victory)
         {
             //enable to force a fight at the start
             //SuperAdventure.Fight(player, new Monster(1, "Goblin", 1, 80, 100));
-            
-            // Main Gameplay loop hierin > bij elke stap checken of er op escape wordt gedrukt dan of menu openen of game afsluiten.
             player.AskPlayerAction(player);
         }
 
--- EpicTextBasedGame/World.cs
+++ EpicTextBasedGame/World.cs
@@ -190,28 +190,21 @@
 
         Location townSquare = new Location(LOCATION_ID_TOWN_SQUARE, "Town square", "You see a fountain.", null, null);
 
-        Location alchemistHut = new Location(LOCATION_ID_ALCHEMIST_HUT, "Alchemist's hut", "There are many strange plants on the shelves.", null, null);
-        alchemistHut.QuestAvailableHere = QuestByID(QUEST_ID_CLEAR_ALCHEMIST_GARDEN);
+        Location alchemistHut = new Location(LOCATION_ID_ALCHEMIST_HUT, "Alchemist's hut", "There are many strange plants on the shelves.", QuestByID(QUEST_ID_CLEAR_ALCHEMIST_GARDEN), null);
 
-        Location alchemistsGarden = new Location(LOCATION_ID_ALCHEMISTS_GARDEN, "Alchemist's garden", "Many plants are growing here.", null, null);
-        alchemistsGarden.MonsterLivingHere = MonsterByID(MONSTER_ID_RAT);
+        Location alchemistsGarden = new Location(LOCATION_ID_ALCHEMISTS_GARDEN, "Alchemist's garden", "Many plants are growing here.", null, MonsterByID(MONSTER_ID_RAT));
 
-        Location farmhouse = new Location(LOCATION_ID_FARMHOUSE, "Farmhouse", "There is a small farmhouse, with a farmer in front.", null, null);
-        farmhouse.QuestAvailableHere = QuestByID(QUEST_ID_CLEAR_FARMERS_FIELD);
+        Location farmhouse = new Location(LOCATION_ID_FARMHOUSE, "Farmhouse", "There is a small farmhouse, with a farmer in front.", QuestByID(QUEST_ID_CLEAR_FARMERS_FIELD), null);
 
-        Location farmersField = new Location(LOCATION_ID_FARM_FIELD, "Farmer's field", "You see rows of vegetables growing here.", null, null);
-        farmersField.MonsterLivingHere = MonsterByID(MONSTER_ID_SNAKE);
+        Location farmersField = new Location(LOCATION_ID_FARM_FIELD, "Farmer's field", "You see rows of vegetables growing here.", null, MonsterByID(MONSTER_ID_SNAKE));
 
-        Location fieldsouth = new Location(LOCATION_ID_FIELD_SOUTH, "Cornfield", "The corn rustles in the wind.", null, null);
-        fieldsouth.MonsterLivingHere = MonsterByID(MONSTER_ID_FACELESS);
+        Location fieldsouth = new Location(LOCATION_ID_FIELD_SOUTH, "Cornfield", "The corn rustles in the wind.", null, MonsterByID(MONSTER_ID_FACELESS));
 
         Location guardPost = new Location(LOCATION_ID_GUARD_POST, "Guard post", "There is a large, tough-looking guard here.", null, null);
 
-        Location bridge = new Location(LOCATION_ID_BRIDGE, "Bridge", "A stone bridge crosses a wide river.", null, null);
-        bridge.QuestAvailableHere = QuestByID(QUEST_ID_COLLECT_SPIDER_SILK);
+        Location bridge = new Location(LOCATION_ID_BRIDGE, "Bridge", "A stone bridge crosses a wide river.", QuestByID(QUEST_ID_COLLECT_SPIDER_SILK), null);
 
-        Location spiderField = new Location(LOCATION_ID_SPIDER_FIELD, "Forest", "You see spider webs covering covering the trees in this forest.", null, null);
-        spiderField.MonsterLivingHere = MonsterByID(MONSTER_ID_GIANT_SPIDER);
+        Location spiderField = new Location(LOCATION_ID_SPIDER_FIELD, "Forest", "You see spider webs covering covering the trees in this forest.", null, MonsterByID(MONSTER_ID_GIANT_SPIDER));
 
         // Link the locations together
         home.LocationToNorth = townSquare;
